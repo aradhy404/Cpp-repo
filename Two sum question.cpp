a00c0175e5789fc188a4fdd5cface6e3776257b6
Aradhy Tripathi <aradhy404@gmail.com>
2026-02-08 22:40:31 +0530

    Optimize twoSum function implementation
    Refactor twoSum function to use a single loop and return indices directly.

--- Two sum question.cpp
+++ Two sum question.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> v;
-        unordered_map<int, int> m;
-        for (int i = 0; i < nums.size(); i++) {
-            m.insert({nums[i], i});
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            int need = target - nums[i];
-            auto it = m.find(need);
-            if (it != m.end() && it->second != i) {
-                v.push_back(it->second);
-                v.push_back(i);
-                break;
+        unordered_map<int, int> v;
+        for(int i = 0;i<nums.size();i++){
+            int k = target-nums[i] ;
+            if(v.find(k)!=v.end()){
+                return {v[k], i};
             }
+            v[nums[i]] = i;
         }
-
-        return v;
+        return {};
     }
 };
